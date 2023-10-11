a1e034ba405901873a49d53631eb293cbbad8ad1
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-10-11 17:11:13 +0100

    create access and save user input, create redirect to sign in page  after signing up

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def home(request):
@@ -7,10 +10,30 @@ def home(request):
 
 
 def signup(request):
+    if request.method == "POST":
+        #username = request.POST.get('username')  NB: You can assign input to field like this like next line.
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created")
+
+        return redirect('signin')
+
     return render(request, "authentication/signup.html")
 
 
 def signin(request):
+
     return render(request, "authentication/signin.html")
 
 
